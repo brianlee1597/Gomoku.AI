8a1a6f1716f84519319d400c7695e14e458c8777
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-04-18 23:52:07 +0900

    array in bounds

--- typescripts/HighlightScores.js
+++ typescripts/HighlightScores.js
@@ -1,10 +1,9 @@
 "use strict";
 var highlightCanvas = document.getElementById('scoredNodeHighlightLayer');
 var highlightCtx = highlightCanvas.getContext('2d');
-var arrayOfColors = ['transparent', 'red', 'blue', 'green', 'violet', 'black'];
+var arrayOfColors = ['transparent', 'red', 'blue', 'green', 'violet', 'yellow', 'grey', 'black'];
 var colorBy = function (score) {
     if (score < arrayOfColors.length)
         return arrayOfColors[score];
-    else
-        throw new Error("error on ColorBy: score is out of Array Bounds");
+    throw new Error("error on ColorBy: score is out of Array Bounds");
 };
--- typescripts/HighlightScores.ts
+++ typescripts/HighlightScores.ts
@@ -1,7 +1,7 @@
 const highlightCanvas = <HTMLCanvasElement> document.getElementById('scoredNodeHighlightLayer')
 const highlightCtx = <CanvasRenderingContext2D> highlightCanvas.getContext('2d')
 
-const arrayOfColors: string[] = ['transparent', 'red', 'blue', 'green', 'violet', 'black']
+const arrayOfColors: string[] = ['transparent', 'red', 'blue', 'green', 'violet', 'yellow', 'grey', 'black']
 
 const colorBy = (score: number): string => {
     if(score < arrayOfColors.length)
